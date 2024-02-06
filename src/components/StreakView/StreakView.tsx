a055f6a79765cb7497601f2899f7ed2101c7c957
Tristan Hollman <tristanhollman@users.noreply.github.com>
2024-02-06 20:45:11 +0100

    Add header and simple empty state

--- src/components/StreakView/StreakView.tsx
+++ src/components/StreakView/StreakView.tsx
@@ -1,17 +1,54 @@
+import { Typography } from "@mui/material";
 import { useConfiguration } from "../../hooks/useConfiguration";
 import "./StreakView.less";
 import { User } from "./User/User";
+import NorthWestIcon from "@mui/icons-material/NorthWest";
 
 export const StreakView = () => {
   const { config } = useConfiguration();
 
-  return (
-    <div className="container">
+  const Header = () => {
+    return (
+      <Typography variant="h2" gutterBottom>
+        Duolingo Streaks
+      </Typography>
+    );
+  };
+
+  const StreakGrid = () => {
+    return (
       <div className="streak-grid">
         {config.userNames.map((userName) => (
           <User userName={userName} key={userName} />
         ))}
       </div>
+    );
+  };
+
+  const EmptyState = () => {
+    const ConfigPointer = () => {
+      return (
+        <div className="pointer">
+          <NorthWestIcon fontSize="large" />
+        </div>
+      );
+    };
+
+    return (
+      <>
+        <Typography variant="subtitle1" gutterBottom>
+          Keep track of your and your friends Duoling streaks!
+        </Typography>
+        <ConfigPointer />
+      </>
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      {config.userNames.length === 0 && <EmptyState />}
+      <StreakGrid />
     </div>
   );
 };
