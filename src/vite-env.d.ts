fafe21d15582f52387793eede8f22d8464ebf4e3
tristanhollman <tristanhollman@users.noreply.github.com>
2024-05-21 20:03:12 +0200

    Add New Relic browser agent (#22)
    * Add new relic configuration
    
    * Add types
    
    * Add empty .env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,18 @@ interface ImportMetaEnv {
    * Used to configure the CORS proxy endpoint, this is set in the github action during the build/deployment.
    */
   readonly VITE_CORS_PROXY_ENDPOINT: string;
+  /**
+   * Used to configure the account id for the new relic browser agent, this is set in the github action during the build/deployment.
+   */
+  readonly VITE_NEW_RELIC_ACCOUNT_ID: string;
+  /**
+   * Used to configure the application id for the new relic browser agent, this is set in the github action during the build/deployment.
+   */
+  readonly VITE_NEW_RELIC_APP_ID: string;
+  /**
+   * Used to configure the license key for the new relic browser agent, this is set in the github action during the build/deployment.
+   */
+  readonly VITE_NEW_RELIC_LICENSE_KEY: string;
 }
 
 interface ImportMeta {
