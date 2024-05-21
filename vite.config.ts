63cbf15e17e8d685728310e7fa9c63f971f584a7
Tristan Hollman <tristanhollman@users.noreply.github.com>
2024-05-21 19:55:30 +0200

    Add plugin to enforce svg url thingy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgLoader()],
   base: "/duolingo-streak-checker",
   css: {
     modules: {
