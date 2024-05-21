346cab2d0ec230f8893434549c7fb9bf5f1c49ed
Tristan Hollman <tristanhollman@users.noreply.github.com>
2024-05-21 21:54:24 +0200

    Fix static asset url handling in vite

--- src/utilities/league-tier-background/determine-league-background.ts
+++ src/utilities/league-tier-background/determine-league-background.ts
@@ -1,39 +1,39 @@
 import { Tier } from "../../entities/models";
-import leagueBronzeSvg from "../../assets/league-tier-backgrounds/bronze.svg?url";
-import leagueSilverSvg from "../../assets/league-tier-backgrounds/silver.svg?url";
-import leagueGoldSvg from "../../assets/league-tier-backgrounds/gold.svg?url";
-import leagueSapphireSvg from "../../assets/league-tier-backgrounds/sapphire.svg?url";
-import leagueRubySvg from "../../assets/league-tier-backgrounds/ruby.svg?url";
-import leagueEmeraldSvg from "../../assets/league-tier-backgrounds/emerald.svg?url";
-import leagueAmethystSvg from "../../assets/league-tier-backgrounds/amethyst.svg?url";
-import leaguePearlSvg from "../../assets/league-tier-backgrounds/pearl.svg?url";
-import leagueObsidianSvg from "../../assets/league-tier-backgrounds/obsidian.svg?url";
-import leagueDiamondSvg from "../../assets/league-tier-backgrounds/diamond.svg?url";
+import leagueBronzeSvg from "../../assets/league-tier-backgrounds/bronze.svg";
+import leagueSilverSvg from "../../assets/league-tier-backgrounds/silver.svg";
+import leagueGoldSvg from "../../assets/league-tier-backgrounds/gold.svg";
+import leagueSapphireSvg from "../../assets/league-tier-backgrounds/sapphire.svg";
+import leagueRubySvg from "../../assets/league-tier-backgrounds/ruby.svg";
+import leagueEmeraldSvg from "../../assets/league-tier-backgrounds/emerald.svg";
+import leagueAmethystSvg from "../../assets/league-tier-backgrounds/amethyst.svg";
+import leaguePearlSvg from "../../assets/league-tier-backgrounds/pearl.svg";
+import leagueObsidianSvg from "../../assets/league-tier-backgrounds/obsidian.svg";
+import leagueDiamondSvg from "../../assets/league-tier-backgrounds/diamond.svg";
 
 export const getLeagueBackgroundImage = (
   currentLeaderboardTier?: number,
 ): string => {
   switch (Tier[currentLeaderboardTier ?? -1]) {
     case "BRONZE":
-      return `url(${leagueBronzeSvg})`;
+      return `url(${new URL(leagueBronzeSvg, import.meta.url).href}`;
     case "SILVER":
-      return `url(${leagueSilverSvg})`;
+      return `url(${new URL(leagueSilverSvg, import.meta.url).href}`;
     case "GOLD":
-      return `url(${leagueGoldSvg})`;
+      return `url(${new URL(leagueGoldSvg, import.meta.url).href}`;
     case "SAPPHIRE":
-      return `url(${leagueSapphireSvg})`;
+      return `url(${new URL(leagueSapphireSvg, import.meta.url).href}`;
     case "RUBY":
-      return `url(${leagueRubySvg})`;
+      return `url(${new URL(leagueRubySvg, import.meta.url).href}`;
     case "EMERALD":
-      return `url(${leagueEmeraldSvg})`;
+      return `url(${new URL(leagueEmeraldSvg, import.meta.url).href}`;
     case "AMETHYST":
-      return `url(${leagueAmethystSvg})`;
+      return `url(${new URL(leagueAmethystSvg, import.meta.url).href}`;
     case "PEARL":
-      return `url(${leaguePearlSvg})`;
+      return `url(${new URL(leaguePearlSvg, import.meta.url).href}`;
     case "OBSIDIAN":
-      return `url(${leagueObsidianSvg})`;
+      return `url(${new URL(leagueObsidianSvg, import.meta.url).href}`;
     case "DIAMOND":
-      return `url(${leagueDiamondSvg})`;
+      return `url(${new URL(leagueDiamondSvg, import.meta.url).href}`;
     default:
       return "";
   }
